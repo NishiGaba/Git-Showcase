489ceea16ed7d2ee5fc8e5904ce7849cec237522
NishiGaba <gabanishi45@gmail.com>
2021-02-15 00:26:25 +0530

    Add Query Widget

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# GithubLoginFirebase
+# Github Showcase Firebase
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.0.4.
 
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { GithubService } from 'src/app/services/github.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-home',
@@ -10,16 +12,26 @@ export class HomeComponent implements OnInit {
 
   user = null;
   userName: string;
+  email= null;
   error = null;
+  queryBlock = false;
+  display = "none";
 
   constructor(
     private githubService: GithubService,
+    private auth: AuthService,
     private ref: ChangeDetectorRef
-    ) { }
+    ) {
+      auth.getUser().subscribe((user) => {
+        if(user != null) {
+          this.email = user.email;
+        }
+      });
+    }
 
   ngOnInit() {
   }
-  
+
   handleFind() {
     this.githubService.getUserDetails(this.userName).subscribe(
       (user) => {
@@ -32,6 +44,26 @@ export class HomeComponent implements OnInit {
         this.error = "User not found.";
       }
     );
+  }
+
+  toggleQueryBlock(flag) {
+    console.log(flag);
+    if(flag == 'open') {
+      this.queryBlock = true;
+    } else {
+      this.queryBlock = false;
+    }
+  }
+
+  openModal() {
+    this.display = "block";
+  }
+  onCloseHandled() {
+    this.display = "none";
+  }
+
+  onSubmit(f: NgForm) {
+
   }
 
 }
